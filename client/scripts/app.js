7ebaff6be94c1bcb13f6057ea2a0fd7cf05797c9
fvntr <phiven@gmail.com>
2015-07-13 15:54:12 -0700

    all messages parsed to prevent scripts from executing

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -11,12 +11,13 @@ var getMessages = function(){
 	  url: 'https://api.parse.com/1/classes/chatterbox',
 	  type: 'GET', //changed to get for updating the DOM
 	  data: JSON, //stringify still parses srcipt, need to find a way to stop parsing
-	  contentType: 'application/json', //refers to json lib
+	  contentType: 'application/jsonp', //refers to jsonp lib, switched in order to break some user input
 	  order: 'createdAt',
 	  success: function (data) { //execute on sucess
 	    console.log(data);
 	    data.results.forEach(function(message){ //iterate over messages 
-	    	$(".messages").append("<p>"+message.createdAt+"</p>"); //put messages on the message Div
+	    	var cleanText = $("<p></p>").text(message.text);
+	    	$(".messages").append(cleanText); //put messages on the message Div
 	    })
 	 //    for(var key in data.results){
 		// $('#messageOutput').append(key.text)
@@ -31,26 +32,24 @@ var getMessages = function(){
 }
 
 var postMessages = function(message){
-		$.ajax({
-	  // This is the url you should use to communicate with the parse API server.
-	  url: 'https://api.parse.com/1/classes/chatterbox',
-	  type: 'POST',
-	  data: JSON.stringify(message),
-	  contentType: 'application/json',
-	  success: function (data) {
-			console.log("chatterbox: Posted");
-	  },
-	  error: function (data) {
-	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	    console.error('chatterbox: Failed to send message');
-	  }
-	});
+		$('.submitButton').on('click', 
+			$.ajax({
+			  // This is the url you should use to communicate with the parse API server.
+			  url: 'https://api.parse.com/1/classes/chatterbox',
+			  type: 'POST', //posting function
+			  data: JSON.stringify(message), //stringifies it 
+			  contentType: 'application/json',
+			  success: function (data) { //executes on success
+					console.log("chatterbox: Posted");
+			  },
+			  error: function (data) { //executes on failed 
+			    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+			    console.error('chatterbox: Failed to send message');
+			  }
+			})
+		)
 }
 
 
 
-setInterval(getMessages(), 200);
-
-$(".submitButton").on("click", function(event){
-    	console.log("chatterbox: was clicked")
-    }) 
+setInterval(getMessages(), 200); //periodically gets new messages
